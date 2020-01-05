62aea898e0ab7c84307ab45ac861883fbc23c472
Raul Flores <raulflo2012@gmail.com>
2020-01-05 10:27:58 -0600

    Add NoteViewModel

--- app/src/main/java/com/example/android/mvvmexample/MainActivity.java
+++ app/src/main/java/com/example/android/mvvmexample/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.android.mvvmexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ask sys for new ViewModel, scopes it to this activity and destroys when this activity destroyed
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
