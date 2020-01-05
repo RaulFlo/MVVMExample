a3cca790c99b6cb7d02436d8e11794a821cb6239
Raul Flores <raulflo2012@gmail.com>
2020-01-05 11:18:02 -0600

    Add RecyclerView

--- app/src/main/java/com/example/android/mvvmexample/MainActivity.java
+++ app/src/main/java/com/example/android/mvvmexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.mvvmexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -18,15 +20,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //reference to the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        //Adapter link to RecyclerView
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         //ask sys for new ViewModel, scopes it to this activity and destroys when this activity destroyed
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                //update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                //update RecyclerView. Every time onChanged is triggered which is every time the data in the table changes
+                adapter.setNotes(notes);
             }
         });
+
+
+
 
     }
 }
